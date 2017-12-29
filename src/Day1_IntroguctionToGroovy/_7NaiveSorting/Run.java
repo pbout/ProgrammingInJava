295bf0cb06b926548dbcd660b51f9f2dc45b1987
pbount <pavlos.boudagidis@gmail.com>
2017-12-29 00:13:22 +0000

    Added exersise 7
    Reason for change:
    * Incomplete exersises
    
    How does this change address the issue:
    * Added solution to exersise 7

--- src/Day1_IntroguctionToGroovy/_7NaiveSorting/Run.java
+++ src/Day1_IntroguctionToGroovy/_7NaiveSorting/Run.java
@@ -1,5 +1,44 @@
 package Day1_IntroguctionToGroovy._7NaiveSorting;
 
+import java.util.Scanner;
+
 public class Run {
-    // TODO
+    public static void main(String[] args) {
+        Run run = new Run();
+        run.start();
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        int n1 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter second number");
+        int n2 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter third number");
+        int n3 = Integer.parseInt(scanner.nextLine());
+
+        if (n1 > n2){
+            if (n2 > n3){
+                int temp = n2;
+                n2 = n3;
+                n3 = temp;
+            }
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
+        }
+
+        if (n2 > n3){
+            if (n1 > n3){
+                int temp = n1;
+                n1 = n3;
+                n3 = temp;
+            }
+            int temp = n2;
+            n2 = n3;
+            n3 = temp;
+        }
+        System.out.println("The numbers in order are: " + n1 + "," + n2 + "," + n3);
+    }
 }
